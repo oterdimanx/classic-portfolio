bc363add0fe4dc538d4fa200e1500d40588f8e03
oterdimanx <olivier@terdiman.fr>
2025-11-17 03:42:41 +0100

    add instagram link in footer

--- src/app/Shop/page.tsx
+++ src/app/Shop/page.tsx
@@ -3,7 +3,6 @@
 import React, { useState , useEffect } from 'react'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
-import Hero from '@/components/Hero'
 import Loading from '../loading'
 import { useDispatch, useSelector } from 'react-redux'
 import { setUserData } from '@/utils/UserDataSlice'
@@ -53,21 +52,30 @@ export default function Shop() {
             <div className="border-r-2 border-r-red-900 flex-initial w-[260px]">
               <p className="mt-3 pt-8 pb-1 pl-9 pr-8 flex-initial text-xl uppercase">Categories</p>
               <ul className="pl-9">
-                <li className="pt-2"><Link href={"/Dashboard"}>Dashboard</Link></li>
-                <li className="pt-2"><Link href={"/Shop"} onClick={()=>SetCategoryId('all')}>ALL</Link></li>
+                <li className="rounded-lg cursor-pointer pt-2 w-[60%] relative">
+                      <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+                      <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900">
+                        <Link href={"/Shop"} onClick={()=>SetCategoryId('all')}>ALL</Link>
+                      </span>
+                  </li>
                 {
                     <>
                         {
-                            catData?.length < 1 ? <h1 className="text-2xl font-semibold text-gray-500">No Categories</h1> :
+                            catData?.length < 1 ? <h1 className="text-2xl font-semibold text-white bg-black text-white px-6 py-3 rounded-lg shadow-md border border-red-400 hover:bg-red-600 hover:text-white transition-colors cursor-pointer">No Categories</h1> :
                             catData?.map((item) => {
-                                return <li className="pt-2" key={'li-' + ii++}><Link 
-                                    href={"/Shop"}
-                                    onClick={()=>{
-                                        SetCategoryId(item?._id)
+                                return <li className="rounded-lg cursor-pointer pt-2 w-[60%] relative" key={'li-' + ii++}>
+                                    <span className="absolute top-0 left-0 mt-1 ml-1 h-full w-full rounded bg-black"></span>
+                                    <span className="fold-bold relative inline-block h-full w-full rounded border-2 border-black bg-white px-3 py-1 text-base font-bold text-black transition duration-100 hover:bg-yellow-400 hover:text-gray-900">
+                                      <Link 
+                                        href={"/Shop"}
+                                        onClick={()=>{
+                                            SetCategoryId(item?._id)
 
-                                    }}
-                                    key={ii++}
-                                >{item?.categoryName}</Link></li>
+                                        }}
+                                        key={ii++}
+                                    >{item?.categoryName}</Link>
+                                  </span>
+                                </li>
                             })
                         }
                     </>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { useRouter } from 'next/navigation';
 
 export default function Footer() {
+  const router = useRouter();
     return (
         <footer className="w-full text-center py-4 text-sm text-neutral-400 border-t border-neutral-700">
         <div className="relative inline">
           © {new Date().getFullYear()} Welcome to NanoMet's Backend Portfolio.
+        </div>
+        <div className="relative inline" onClick={() => router.push(`https://www.instagram.com/rouks.dome`)}>
+          <button id="instagram" className=" border-2 hover:border-0 border-pink-500 bg-gradient-to-b text-2xl hover:from-indigo-600 hover:via-pink-600 hover:to-yellow-500 min-w-wull hover:text-white bg-white text-pink-600 w-12 h-12  transform hover:-translate-y-3 rounded-full duration-500 ">
+            <FontAwesomeIcon icon={faInstagram} />
+          </button>
         </div>
         <div className="relative inline">
           <div className="front relative bg-[gold] text-lime-500 text-[8vh] font-black font-serif">
